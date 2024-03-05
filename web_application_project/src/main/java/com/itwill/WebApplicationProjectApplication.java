174cf5f9129f33e0f86fc3aab9de5c7f3adf2164
vnfans44718 <vnfans44718@gmail.com>
2024-03-05 14:17:46 +0900

    ..

---
+++ web_application_project/src/main/java/com/itwill/WebApplicationProjectApplication.java
@@ -0,0 +1,13 @@
+package com.itwill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplicationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApplicationProjectApplication.class, args);
+	}
+
+}
